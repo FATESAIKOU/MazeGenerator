42c88a313e29a95bec5b8c5c3a44cefe24c23274
FATESAIKOU <qzsecftbhhhh@gmail.com>
2017-10-24 22:18:02 +0800

    genMaze: ver2.0 complete

--- src/genMaze.py
+++ src/genMaze.py
@@ -2,18 +2,21 @@ import random
 import os
 import sys
 import numpy as np
+import time
 from pprint import pprint
 
 
 def getReachable(x, y, n, m, maze):
     eles = [
+        (x, y - 1),
         (x, y + 1),
-        (x + 1, y)
+        (x + 1, y),
+        (x - 1, y),
     ]
 
     next_set = set()
     for (nx, ny) in eles:
-        if -1 < nx < n and -1 < ny < m and maze[nx][ny] == 0:
+        if -1 < nx < n and -1 < ny < m and maze[nx][ny] == 0 and isVisitable(nx, ny, n, m, maze):
             next_set.add((nx, ny))
 
     return next_set
@@ -29,7 +32,7 @@ def isVisitable(x, y, n, m, maze):
     lnk_cnt = 0
     for (nx, ny) in eles:
         if -1 < nx < n and -1 < ny < m:
-            lnk_cnt += maze[nx, ny]
+            lnk_cnt += int(maze[nx, ny]) & 1
 
     return lnk_cnt == 1
     
@@ -41,8 +44,11 @@ def genMaze(x, y, n, m):
     maze[nx][ny] = 1
 
     next_set = set()
-    plen = random.randint(200, 200)
+    plen = 1
     while True:
+        os.system('clear')
+        printMaze(35, 70, maze, ('  ', '##'))
+
         tmp_set = getReachable(nx, ny, n, m, maze);
         next_set = next_set.union(tmp_set)
 
@@ -53,7 +59,7 @@ def genMaze(x, y, n, m):
             aim_set = tmp_set
             plen = plen - 1
         else:
-            plen = random.randint(9, 10)
+            plen = 1 << random.randint(1, 7)
             aim_set = next_set
 
         nx, ny = random.sample(aim_set, 1)[0]
@@ -61,13 +67,13 @@ def genMaze(x, y, n, m):
 
         if isVisitable(nx, ny, n, m, maze):
             maze[nx][ny] = 1
-        else:
-            print >> sys.stderr, maze[nx][ny], (nx, ny)
+            time.sleep(0.01)
+        
 
     return maze
 
 def printMaze(n, m, maze, draw_pair):
-    print " " + "_"*m + " "
+    print " " + "_"*m*len(draw_pair[0]) + " "
     for i in range(n):
         s = "|"
         for j in range(m):
@@ -76,6 +82,8 @@ def printMaze(n, m, maze, draw_pair):
             else:
                 s = s + draw_pair[1]
         print s + "|"
-    print " " + "-"*m + " "
+    print " " + "-"*m*len(draw_pair[0]) + " "
 
-printMaze(35, 140, genMaze(0, 0, 35, 140), ('1', '0'))
+
+while True:
+    genMaze(0, 0, 35, 70)
