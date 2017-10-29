43aa8c8c9cdcb551899c3295dbde78c262e3d31e
FATESAIKOU <qzsecftbhhhh@gmail.com>
2017-10-29 22:12:15 +0800

    add coin to maze gen

--- src/genMaze.py
+++ src/genMaze.py
@@ -32,12 +32,12 @@ def isVisitable(x, y, n, m, maze):
     ]
 
     lnk_cnt = sum([
-        maze[nx, ny] 
+        maze[nx, ny] != 0
         for nx, ny in eles 
         if -1 < nx < n and -1 < ny < m
     ])
 
-    return lnk_cnt == 1
+    return lnk_cnt == 1 or (((n - 1, m - 1) in eles or (x, y) == (n - 1, m - 1)) and maze[n - 1, m - 1] == 0)
 
 def genMaze(x, y, n, m):
     maze = np.zeros([n, m])
@@ -60,12 +60,12 @@ def genMaze(x, y, n, m):
         nx, ny = random.sample(aim_set, 1)[0]
 
         if isVisitable(nx, ny, n, m, maze):
-            maze[nx, ny] = 1
+            maze[nx, ny] = 1 << random.randint(1, 10) / 8
 
-            os.system('clear')
-            printMaze(35, 70, maze, ('  ', '##'), next_set, tmp_set)
-            print plen, tmp_set
-            time.sleep(0.1)
+            #os.system('clear')
+            #printMaze(n, m, maze, ('  ', '##'), next_set, tmp_set)
+            #print plen, tmp_set
+            #time.sleep(0.01)
 
         next_set = next_set.union(aim_set)
         next_set.discard((nx, ny))
@@ -78,21 +78,25 @@ def genMaze(x, y, n, m):
 
 def printMaze(n, m, maze, draw_pair, next_set, tmp_set):
     print " " + "_"*m*len(draw_pair[0]) + " "
+
+    s = ""
     for i in range(n):
-        s = "|"
+        s = s + "|"
         for j in range(m):
             if maze[i, j] == 1:
                 s = s + draw_pair[0]
+            elif maze[i, j] == 2:
+                s = s + draw_pair[1]
             else:
                 if (i, j) in next_set:
                     s = s + "\x1B[32m" + draw_pair[1] + "\x1B[37m"
                 elif (i, j) in tmp_set:
                     s = s + "\x1B[31m" + draw_pair[1] + "\x1B[37m"
                 else:
-                    s = s + draw_pair[1]
+                    s = s + draw_pair[2]
 
-        print s + "|"
-    print " " + "-"*m*len(draw_pair[0]) + " "
+        s = s + "|\n"
+    print s + " " + "-"*m*len(draw_pair[0]) + " "
 
-while True:
-    genMaze(0, 0, 35, 70)
+
+printMaze(10, 20, genMaze(0, 0, 10, 20), ('1', '2', '0'), set(), set())
